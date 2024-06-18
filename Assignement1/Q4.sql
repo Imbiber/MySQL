da5464070b97a69b259e5a2e1f6ea43c1c43a4ea
Nishant Gaurav <ng19nishant@gmail.com>
2024-06-18 22:29:34 +0530

    Department table

---
+++ Assignement1/Q4.sql
@@ -0,0 +1,12 @@
+CREATE TABLE DEPARTMENT(
+DEPT_NAME VARCHAR(20),
+BUILDING VARCHAR(20),
+BUDGET NUMERIC(10))
+INSERT INTO DEPARTMENT VALUES
+('Biology','Watson',90000),
+('Comp. Sci.','Taylor',100000),
+('Elec. Eng.','Taylor',85000),
+('Finance','Painter',120000),
+('History','Painter',50000),
+('Music','Packard',80000),
+('Physics','Watson',70000);
